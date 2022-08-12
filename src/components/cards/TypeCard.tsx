0cecfeb36b3052e1cca9bc8653a3ae5b02060d72
Agustin Cristobo <fr33yr@gmail.com>
2022-08-12 17:48:45 -0300

    ..

--- src/components/cards/TypeCard.tsx
+++ src/components/cards/TypeCard.tsx
@@ -9,11 +9,12 @@ export function TypeCard({name, imgUrl}:CardItempProps) {
 
     return (
         <>
-            <div className='flex flex-col w-fit h-fit drop-shadow-sm cursor-pointer'>
+            <div className='flex flex-col w-fit h-fit drop-shadow-sm cursor-pointer
+            text-stone-600 hover:text-white'>
                 <img src={imgUrl} alt="ring-img" height="153" width="207"
                     className='max-w-full h-auto rounded-t' />
                 <div className='flex flex-col bg-stone-900 rounded-b p-3'>
-                    <p className='text-stone-600'>{name}</p>
+                    <p className=''>{name}</p>
                 </div>
             </div>
         </>
