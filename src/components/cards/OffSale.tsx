b2915b91b1c5c4f27733656dac7a21c6fbd983c9
Agustin Cristobo <fr33yr@gmail.com>
2022-08-30 19:31:35 -0300

    offsale styles

--- src/components/cards/OffSale.tsx
+++ src/components/cards/OffSale.tsx
@@ -14,12 +14,13 @@ export function OffSale({imgUrl, pricing, _id, name, description}: IData) {
   return (
     <>
         <div className='flex flex-col w-fit h-fit'>
-            <div className="flex flex-row justify-between" style={{maxWidth: "304px"}}>
+            <div className="flex flex-row justify-between bg-stone-900
+            rounded-t-md px-4 pb-1" style={{maxWidth: "304px"}}>
                 <p className='text-stone-600'>Off sale</p>
                 <p className='text-white'><s className='pr-4'>${pricing.price}</s>  ${pricing.offsale} </p>
             </div>
             <img src={imgUrl} alt="ring-img" height='210' width='210'
-            className='rounded cursor-pointer' onClick={handleOnClick}/>
+            className='rounded-b-md cursor-pointer' onClick={handleOnClick}/>
         </div>
     </>
   )
